cf6cb66d74fbc13cfed6c2259caa8c4d7035621a
amtksms97 <amtksms97@gmail.com>
2020-12-17 22:56:31 +0530

    Added code to check whether the first name is valid or not

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -1,8 +1,22 @@
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class UserRegistration {
-
+	Scanner scanner=new Scanner(System.in);
+	String firstName;
+	public UserRegistration() {
+		this.firstName=null;
+	}
+	public void checkFirstName()
+	{
+		System.out.println("Enter the first name:");
+		firstName=scanner.next();
+		System.out.println(Pattern.matches("[A-Z][a-z]{3,}", firstName));
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Program");
+		UserRegistration object=new UserRegistration();
+		object.checkFirstName();
 
 	}
 
