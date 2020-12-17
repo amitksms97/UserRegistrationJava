205368e564b380b8b53cc1488a4bc5f88d1f5523
amtksms97 <amtksms97@gmail.com>
2020-12-17 23:04:35 +0530

    Added code to check second name

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	Scanner scanner=new Scanner(System.in);
 	String firstName;
+	String secondName;
 	public UserRegistration() {
 		this.firstName=null;
 	}
@@ -13,10 +14,17 @@ public class UserRegistration {
 		firstName=scanner.next();
 		System.out.println(Pattern.matches("[A-Z][a-z]{3,}", firstName));
 	}
+	public void checkSecondName()
+	{
+		System.out.println("Enter the second name:");
+		secondName=scanner.next();
+		System.out.println(Pattern.matches("[A-Z][a-z]{3,}", secondName));
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Program");
 		UserRegistration object=new UserRegistration();
 		object.checkFirstName();
+		object.checkSecondName();
 
 	}
 
