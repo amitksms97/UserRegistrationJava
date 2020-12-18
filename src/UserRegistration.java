e47a6891f23e82ae998bd360a3329c5aaed89995
amtksms97 <amtksms97@gmail.com>
2020-12-18 10:48:05 +0530

    Added code to check for valid phone number

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -6,10 +6,12 @@ public class UserRegistration {
 	String firstName;
 	String secondName;
 	String email;
+	String phoneNumber;
 	public UserRegistration() {
 		this.firstName=null;
 		this.secondName=null;
 		this.email=null;
+		this.phoneNumber=null;
 	}
 	public void checkFirstName()
 	{
@@ -29,12 +31,20 @@ public class UserRegistration {
 		email=scanner.next();
 		System.out.println(Pattern.matches("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", email));
 	}
+	public void checkPhoneNumber()
+	{
+		System.out.println("Enter the phone number:");
+		phoneNumber=scanner.next();
+		//System.out.println(Pattern.matches("^[91]{2}[\s][6-9][0-9]{9}", phoneNumber));
+		System.out.println(Pattern.matches("^[91]{2}[6-9][0-9]{9}", phoneNumber));
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Program");
 		UserRegistration object=new UserRegistration();
 		object.checkFirstName();
 		object.checkSecondName();
 		object.checkEmail();
+		object.checkPhoneNumber();
 	}	
 
 }
